648434eb32f7e73adf499995c38c24cf79613ea1
Yvan TAMBAT <tambat.yvan18@gmail.com>
2026-03-09 08:41:35 +0100

    premier commit

---
+++ src/main/java/com/deployfast/backend/DeployfastBackendApplication.java
@@ -0,0 +1,13 @@
+package com.deployfast.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeployfastBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeployfastBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/deployfast/backend/DeployfastBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deployfast.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeployfastBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
