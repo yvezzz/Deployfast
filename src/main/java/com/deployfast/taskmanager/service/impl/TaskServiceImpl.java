019240f499f3e599c79cd0e194d2f115a5c5e34e
Yvan TAMBAT <tambat.yvan18@gmail.com>
2026-03-11 13:27:46 +0100

    Pipeline update

--- src/main/java/com/deployfast/taskmanager/service/impl/TaskServiceImpl.java
+++ src/main/java/com/deployfast/taskmanager/service/impl/TaskServiceImpl.java
@@ -14,7 +14,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 /**
- * Implémentation du service de gestion des tâches
+ * Implémentation du service de gestion des tâches.
  */
 @Service
 @RequiredArgsConstructor
